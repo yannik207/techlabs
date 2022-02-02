1be676c47a03db0026b6e134099df4c08e5d63d1
yannik207 <ysacherer@icloud.com>
2022-02-02 20:39:40 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# ============================================================
+# file:            Kundendaten
+# Author:            Yannik Sacherer
+# necessary packages: Pandas, Numpy, Datatile
+# ============================================================
+
+
+#load packages --------------------------
+import pandas as pd
+import numpy as np
+from datatile.summary.df import DataFrameSummary
+#load data --------------------------------------
+df = pd.read_csv('/Users/yanniksa/Desktop/bangladesh.csv', sep=';')
+
+#Data Overview --------------------------------------
+print(df.head(5))
+df_summary = DataFrameSummary(df)
+print(df_summary.summary())
+#explorative analysis --------------------------------------
+
+#Data preperation --------------------------------------
+
+#Modelling --------------------------------------
+
+#Data Understanding --------------------------------------
+
+#Evaluation --------------------------------------
