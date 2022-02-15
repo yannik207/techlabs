3b0befdb2084961ee4a2edf40cfcfc361249e567
yannik207 <ysacherer@icloud.com>
2022-02-15 16:55:36 +0100

    changed Latitude

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 ###load packages --------------------------
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from datatile.summary.df import DataFrameSummary
 ###load data --------------------------------------
 df = pd.read_csv('/Users/yanniksa/Desktop/bangladesh.csv', sep=';')
@@ -16,7 +17,7 @@ df = pd.read_csv('/Users/yanniksa/Desktop/bangladesh.csv', sep=';')
 
 df_summary = DataFrameSummary(df)
 print(df_summary.summary())
-
+print(df)
 ###Data Cleaning ---------------------------------------
 # to handle missing values in the column of 'Flood?'. we fill in 0
 df['Flood?'] = df['Flood?'].fillna(0)
@@ -24,14 +25,41 @@ df.isnull().values.any()
 
 # Latitude has wrong values. change it with domain knowledge
 # Observation 0 to 1355 has the value of '22. Juli' instead of 22.17
+
 df['LATITUDE'] = df['LATITUDE'].replace('22. Jul', 22.17)
+print(df.head(5))
+
+
+# remove string from temp observations
+df['Max_Temp'] = df['Max_Temp'].str.extract('(\d+)')
+df['Min_Temp'] = df['Min_Temp'].str.extract('(\d+)')
+df['Cloud_Coverage'] = df['Cloud_Coverage'].str.extract('(\d+)')
+print(df.info())
+df['Cloud_Coverage'] = df['Cloud_Coverage'].astype('int64')
+df['Max_Temp'] = df['Max_Temp'].astype('int64')
+df['Min_Temp'] = df['Min_Temp'].astype('int64')
+df['Rainfall'] = df['Rainfall'].str.extract('(\d+)')
+df['Rainfall'] = df['Rainfall'].astype('int64')
+df['Relative_Humidity'] = df['Relative_Humidity'].str.extract('(\d+)')
+df['Relative_Humidity'] = df['Relative_Humidity'].astype('int64')
+
+del df['Bright_Sunshine']
+
+num_variable = ['Sl', 'Year', 'Month', 'Max_Temp', 'Min_Temp', 'Rainfall',
+                'Relative_Humidity', 'Cloud_Coverage', 'Station_Number', 'X_COR', 'Y_COR', 'LONGITUDE', 'ALT', 'Period', 'Flood?']
+for i in num_variable:
+    print(i)
+
+
+for i in num_variable:
+    plt.figure()
+    plt.hist(df[i])
+    plt.title(i)
+    plt.show()
 
 
 
 ###explorative analysis --------------------------------------
-
-
-
 
 ###Data preperation --------------------------------------
 
