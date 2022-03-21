862d83254ddcbd477e35fc58d3852c4ce29941e3
helena2303 <92525925+helena2303@users.noreply.github.com>
2022-03-21 20:57:47 +0100

    Add files via upload

--- Prediction.py
+++ Prediction.py
@@ -7,13 +7,13 @@ prediction = flood()
 
 prediction.data = pd.read_csv('Bangladesh.csv', sep=';', encoding= 'unicode_escape')
 prediction.place = 'Bhola' #when all=False use this station
-prediction.feature = ['Rainfall','Cloud_Coverage','Bright_Sunshine','Wind_Speed','Max_Temp','Min_Temp','Relative_Humidity'] #'Cloud_Coverage','Bright_Sunshine','Wind_Speed','Max_Temp','Min_Temp','Relative_Humidity','Sl','Year','Month', 'Station_Number', 'X_COR', 'Y_COR', 'LATITUDE', 'LONGITUDE', 'ALT', 'Period'
+prediction.feature = ['Rainfall','Cloud_Coverage','Bright_Sunshine','Wind_Speed','Max_Temp','Min_Temp','Relative_Humidity'] #features used. Possible:'Cloud_Coverage','Bright_Sunshine','Wind_Speed','Max_Temp','Min_Temp','Relative_Humidity','Sl','Year','Month', 'Station_Number', 'X_COR', 'Y_COR', 'LATITUDE', 'LONGITUDE', 'ALT', 'Period'
 prediction.test_size = 0.3
 
 prediction.preprocess()     #clear Dataset
-prediction.split()          #split into test ans train
+prediction.split()          #split into test and train
 
-#prediction.LR_train(all=True)         #Logistic Regression train all stations, test all stations
+#prediction.LR_train(all=True)         #Logistic Regression
 #prediction.LR_prediction(all=True)
 
 prediction.Random_Forest()
--- flood_class2.py
+++ flood_class2.py
@@ -22,13 +22,15 @@ class flood(object):
 		self.test_size=kwargs.get("test_size", 0.8)
 
 
+	#cleaning Dataset
 	def preprocess(self):
-        #ceep only relevant columns in the dataset 
+        #sort Dataset by time for correct splitting
 		self.data = self.data.sort_values(by=['Period'])
 
+		#ceep only relevant columns in the dataset 
 		self.data = self.data[['Station_Names']+self.feature+['Flood?']]
 
-        #replace , in dataset
+        #replace , in dataset with .
 		for i in self.feature:
 			self.data[i]=self.data[i].str.replace(',','.')
 
@@ -47,27 +49,34 @@ class flood(object):
 
 		groups = self.data.groupby(self.data.Station_Names)
 
-		#for i in names:
-		#	stations = (groups.get_group(i))
-		#	self.data_stations[i] = [stations.iloc[:,1:-1], stations.iloc[:,-1]]
-
 		stations = (groups.get_group(self.place))
 		self.data_stations[self.place] = [stations.iloc[:,1:-1], stations.iloc[:,-1]]
 
+		#drop single station to do not train model when testing with single station
+		self.data.drop(self.data[self.data['Station_Names'] == self.place].index, inplace=True)
+
+		#drop station names
 		self.data = self.data.drop(columns=['Station_Names'])
+
+		#split in data (x) and label (y)
 		self.x=self.data.iloc[:,:-1]
 		self.y=self.data.iloc[:,-1]
 
-
+	#split in test and training set
 	def split(self):
-
+		# split randomly with given test size
 		self.x_train,self.x_test,self.y_train,self.y_test=train_test_split(self.x,self.y,test_size=self.test_size)
+
+		#save train lenght
 		len_train = len(self.x_train)
+
+		# split in test and train with same train lenght but note we use time series -> test data temporal after train data
 		self.x_train = self.x.iloc[:len_train]
 		self.x_test = self.x.iloc[len_train:]
 		self.y_train = self.y.iloc[:len_train]
 		self.y_test = self.y.iloc[len_train:]
 
+		#same with single station
 		self.x_train2,self.x_test2,self.y_train2,self.y_test2=train_test_split(self.data_stations[self.place][0],self.data_stations[self.place][1],test_size=self.test_size)
 		len_train = len(self.x_train2)
 		self.x_train2 = self.data_stations[self.place][0].iloc[:len_train]
@@ -83,8 +92,10 @@ class flood(object):
 		self.y_train2=self.y_train2.astype('int')
 		self.y_test2=self.y_test2.astype('int')
         
+
 	def feature_importance(self,lr):
         
+		#feature importance are coefficents of model
 		importance = lr.coef_[0]
 		for i,v in enumerate(importance):
 			print('Feature {}: {}, Score: {}'.format(i,self.feature[i],np.round(v,2)))
@@ -100,9 +111,12 @@ class flood(object):
 		plt.show()
 
 
+	#train model with logistic regression
 	def LR_train(self,all):
 		# Scaling the data between 0 and 1.
 		minmax = preprocessing.MinMaxScaler(feature_range=(0,1))
+
+		# decide if all stations or one station
 		if all == True:
 			minmax.fit(self.x).transform(self.x)
 			x_train = self.x_train2
@@ -113,33 +127,38 @@ class flood(object):
 			y_train = self.y_train
 		
         
-		x_train_std=minmax.fit_transform(x_train)         # fit the values in between 0 and 1.
+		# fit the values in between 0 and 1. (for normalization)
+		x_train_std=minmax.fit_transform(x_train)         
 
-
+		# define Logistic Regression and Hyperparameter optimization
 		lr=LogisticRegression()
-		my_cv = TimeSeriesSplit(n_splits=10).split(x_train_std)
+		my_cv = TimeSeriesSplit(n_splits=10).split(x_train_std) # split in train and validation set with rolling method
 		space = dict()
 		space['solver'] = ['newton-cg', 'lbfgs', 'liblinear']
 		space['penalty'] = ['none', 'l1', 'l2', 'elasticnet']
 		space['C'] = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100]
 		search = GridSearchCV(lr, space, scoring='roc_auc', n_jobs=-1,cv=my_cv)
 
-		self.result = search.fit(x_train_std,y_train)	 # train the model based on full dataset x&y
+		# train model with best hyperparamters
+		self.result = search.fit(x_train_std,y_train)	 
 
 		print('Best Score: %s' % self.result.best_score_)
 		print('Best Hyperparameters: %s' % self.result.best_params_)
 
+		# perform feature importance analysis
 		self.feature_importance(self.result.best_estimator_)
 
+		# save trained model
 		filename = 'logistic_regression_model.sav'
 		pickle.dump(self.result.best_estimator_, open(filename, 'wb'))
 
+		# print if all stations or one station is used
 		if all == True:
 			print("train all stations")
 		if all == False:
 			print("train one station")
 		
-		
+	#test model
 	def LR_prediction(self, all):
 		try:
 			filename = 'logistic_regression_model.sav'
@@ -147,9 +166,11 @@ class flood(object):
 		except:
 			model = self.result.best_estimator_
 
+		#define normalization
 		minmax = preprocessing.MinMaxScaler(feature_range=(0,1))
 		minmax.fit(self.x).transform(self.x)
 
+		# decide if all stations or one station
 		if all == False:
 			x_test = self.x_test2
 			y_test = self.y_test2
@@ -158,25 +179,36 @@ class flood(object):
 			x_test = self.x_test
 			y_test = self.y_test
 
+		# fit the values in between 0 and 1. (for normalization)
 		x_test_std=minmax.fit_transform(x_test)
+
+		# predict with test x values
 		y_pred=model.predict(x_test_std)
         
+		# print if all or one station is used for testing
 		if all == True:
 			print("test all stations")
 		if all == False:
 			print("test one station")
+
+		# calculate accuracy with comparison of prediction and label
 		print("\naccuracy score:%f"%(accuracy_score(y_test,y_pred)*100))
 		print("recall score:%f"%(recall_score(y_test,y_pred)*100))
 		print("roc score:%f"%(roc_auc_score(y_test,y_pred)*100))
 		print(confusion_matrix(y_test,y_pred))
 
+
+	#Random Forrest model
 	def Random_Forest(self):
+		# Define for normalization
 		minmax = preprocessing.MinMaxScaler(feature_range=(0,1))
 		minmax.fit(self.x).transform(self.x)
 
-		x_train_std=minmax.fit_transform(self.x_train)         # fit the values in between 0 and 1.
+		#Fit values between 0 and 1 for normalization
+		x_train_std=minmax.fit_transform(self.x_train)      
 		x_test_std=minmax.fit_transform(self.x_test)
 
+		# Define Random Forest model
 		rmf=RandomForestClassifier(max_depth=3,random_state=0)
 
 		# Number of trees in random forest
@@ -201,31 +233,29 @@ class flood(object):
                'min_samples_leaf': min_samples_leaf,
                'bootstrap': bootstrap}
 
-		'''
+		# Split in train and validation set with rolling method
+		my_cv = TimeSeriesSplit(n_splits=3).split(x_train_std)
+
+		# Hyperparamteter optimization
 		rmf_random = RandomizedSearchCV(estimator=rmf, param_distributions=random_grid,
                               n_iter = 100, scoring='roc_auc', 
-                              cv = TimeSeriesSplit(n_splits=3), verbose=2, random_state=42, n_jobs=-1,
+                              cv = my_cv, verbose=2, random_state=42, n_jobs=-1,
                               return_train_score=True)
-		'''
-		print('start Random forrest')
-		my_cv = TimeSeriesSplit(n_splits=3).split(x_train_std)
-		print('startGridSearch')
-		rmf_random = GridSearchCV(estimator=rmf, param_grid=random_grid, cv=my_cv)
-		print('start fit')
+		#rmf_random = GridSearchCV(estimator=rmf, param_grid=random_grid, cv=my_cv)
+
+		# Train model
 		rmf_clf=rmf_random.fit(x_train_std,self.y_train)
-		print('finished')
+
 		print(rmf_random.best_params_)
 
 		#rmf_clf_acc=cross_val_score(rmf_clf,x_train_std,self.y_train,cv=3,scoring="accuracy",n_jobs=-1)
 		#rmf_proba=cross_val_predict(rmf_clf,x_train_std,self.y_train,cv=3,method='predict_proba')
 
-		y_pred=rmf_clf.best_estimator_.predict(x_test_std)
-
-		importances = rmf_clf.best_estimator_.feature_importances_ #Feature importances are provided by the fitted attribute feature_importances_ and they are computed as the mean and standard deviation of accumulation of the impurity decrease within each tree.
-		#std = np.std([tree.feature_importances_ for tree in rmf.estimators_], axis=0)
-
+		#Perform Feature importance
+		importances = rmf_clf.best_estimator_.feature_importances_ 
 		forest_importances = pd.Series(importances, index=self.feature)
 
+		#plot feature imortance
 		fig, ax = plt.subplots()
 		forest_importances.plot.bar(ax=ax)
 		ax.set_title("Feature importances using MDI")
@@ -233,11 +263,17 @@ class flood(object):
 		fig.tight_layout()
 		plt.show()
 
+		# Predict with test x values
+		y_pred=rmf_clf.best_estimator_.predict(x_test_std)
+
+		#calculate accuracy with comparison of prediction and label
 		#print("\naccuracy score:%f"%(accuracy_score(self.y_test,y_pred)*100))
 		#print("recall score:%f"%(recall_score(self.y_test,y_pred)*100))
 		print("roc score:%f"%(roc_auc_score(self.y_test,y_pred)*100))
 		print(confusion_matrix(self.y_test,y_pred))
 
+
+	# Lade trainiertes Modell und wende es auf einen neuen Datensatz an (nicht benutzt)
 	def new(self):
 		
         #replace , in dataset
